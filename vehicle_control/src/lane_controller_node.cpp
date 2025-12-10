b5a55c05a7a5e0496ea0eee184690372e900d0f9
ErsaGunTosun <ersagunfatihdev@gmail.com>
2025-12-10 11:34:12 +0000

    ADD lane follow perception and control lane follow perception node and control node added

---
+++ vehicle_control/src/lane_controller_node.cpp
@@ -0,0 +1,51 @@
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "geometry_msgs/msg/twist.hpp" 
+
+using std::placeholders::_1;
+
+class LaneController : public rclcpp::Node
+{
+public:
+  LaneController() : Node("lane_controller")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::Float32>(
+      "/lane/error", 10, std::bind(&LaneController::topic_callback, this, _1));
+
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    
+    kp_ = 0.005; 
+    base_speed_ = 0.5; 
+  }
+
+private:
+  void topic_callback(const std_msgs::msg::Float32::SharedPtr msg) const
+  {
+    float error = msg->data;
+    float steering_angle = -1.0 * kp_ * error; 
+
+    if (steering_angle > 0.5) steering_angle = 0.5;
+    if (steering_angle < -0.5) steering_angle = -0.5;
+
+    auto cmd_msg = geometry_msgs::msg::Twist();
+    cmd_msg.linear.x = base_speed_;
+    cmd_msg.angular.z = steering_angle;
+
+    publisher_->publish(cmd_msg);
+  }
+
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr subscription_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  
+  double kp_;
+  double base_speed_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<LaneController>());
+  rclcpp::shutdown();
+  return 0;
+}
--- vehicle_perception/src/lane_follow_node.cpp
+++ vehicle_perception/src/lane_follow_node.cpp
@@ -1,22 +1,19 @@
 #include "rclcpp/rclcpp.hpp"
-#include <opencv2/opencv.hpp>
-#include "cv_bridge/cv_bridge.h"
-#include "sensor_msgs/msg/image.hpp"
-#include <opencv2/highgui/highgui.hpp>
 
+#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "cv_bridge/cv_bridge.h"
 
 class LaneFollow: public rclcpp::Node
 {
     public:
     LaneFollow(): Node("lane_follow")
     {
-        rclcpp::QoS qos_profile = rclcpp::SensorDataQoS();
-
-        subscriber_ = create_subscription<sensor_msgs::msg::Image>(
-            "/camera", 
-            qos_profile,     
-            std::bind(&LaneFollow::image_callback, this, std::placeholders::_1));
-
+        timer_ = create_wall_timer(std::chrono::milliseconds(100), std::bind(&LaneFollow::start_operations, this));
+      
         cv::namedWindow("CAMERA");
     }
 
@@ -27,6 +24,30 @@ class LaneFollow: public rclcpp::Node
 
     private:
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+
+    const cv::Scalar LOWER_COLOR_1 = cv::Scalar(0, 100, 100);    
+    const cv::Scalar UPPER_COLOR_1 = cv::Scalar(10, 255, 255); 
+    const cv::Scalar LOWER_COLOR_2 = cv::Scalar(160, 100, 100); 
+    const cv::Scalar UPPER_COLOR_2 = cv::Scalar(180, 255, 255); 
+
+    const int ROI_HEIGHT_RATIO = 5; 
+
+
+    bool is_start = false;
+    
+    void start_operations(){
+        subscriber_ = create_subscription<sensor_msgs::msg::Image>(
+            "/camera", 
+            10,     
+            std::bind(&LaneFollow::image_callback, this, std::placeholders::_1));
+
+        publisher_ = this->create_publisher<std_msgs::msg::Float32>("/lane/error", 10);
+
+        is_start = true;
+    }
 
     void image_callback(const sensor_msgs::msg::Image::SharedPtr img)
     {
@@ -46,7 +67,65 @@ class LaneFollow: public rclcpp::Node
             return;
         }
 
-        cv::imshow("CAMERA", img_bgr);
+        int roi_start_row = img_bgr.rows / 2; 
+        cv::Mat roi_img = img_bgr(cv::Range(roi_start_row, img_bgr.rows), cv::Range::all());
+
+        cv::Mat hsv_image;
+        cv::cvtColor(roi_img, hsv_image, cv::COLOR_BGR2HSV);
+
+        cv::Mat mask_1, mask_2, mask; 
+        cv::inRange(hsv_image, LOWER_COLOR_1, UPPER_COLOR_1, mask_1);
+        cv::inRange(hsv_image, LOWER_COLOR_2, UPPER_COLOR_2, mask_2);
+        cv::bitwise_or(mask_1, mask_2, mask);
+
+        cv::bitwise_not(mask, mask);
+
+        cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+        cv::erode(mask, mask, kernel);
+        cv::dilate(mask, mask, kernel);
+
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        double max_area = 0;
+        int max_idx = -1;
+
+        for (size_t i = 0; i < contours.size(); i++) {
+            double area = cv::contourArea(contours[i]);
+            if (area > max_area) {
+                max_area = area;
+                max_idx = i;
+            }
+        }
+
+        double err = 0.0;
+        bool road_found = false;
+
+        cv::Mat debug_view;
+        cv::cvtColor(mask, debug_view, cv::COLOR_GRAY2BGR);
+
+        if (max_idx != -1 && max_area > 1000) 
+        {
+            road_found = true;
+            
+            cv::Moments M = cv::moments(contours[max_idx]);
+            if (M.m00 > 0) {
+                int cx = int(M.m10 / M.m00);
+                int cy = int(M.m01 / M.m00);
+
+                int width = roi_img.cols;
+                err = cx - (width / 2);
+
+                auto msg = std_msgs::msg::Float32();
+                msg.data = err;
+                publisher_->publish(msg);
+
+                cv::drawContours(debug_view, contours, max_idx, cv::Scalar(0, 255, 0), 2);
+                cv::circle(debug_view, cv::Point(cx, cy), 10, cv::Scalar(0, 0, 255), -1);
+            }
+        }
+
+        cv::imshow("CAMERA", debug_view);
         cv::waitKey(1);
     }
 
